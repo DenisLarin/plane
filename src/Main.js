9cd7bc0b7f592f0c2bae70167cdf6417f12a6161
Denis Larin <mr.denislarin@icloud.com>
2019-09-23 03:22:38 +0300

    init

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Map from "./containers/map/MapScreen";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Map/>
+            </div>
+        );
+    }
+}
+
+export default Main;
---
+++ src/containers/map/MapScreen.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+
+class MapScreen extends Component {
+    componentDidMount() {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZGVuaXNsYXJpbiIsImEiOiJjazB2bjJ5cnQwNmNmM25ueHprc2JvdzRwIn0.NHSA0JPYqDe1rw5MdQuouA';
+        var map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            // center: [55.753215, 37.622504],
+            zoom: 10
+        });
+    }
+
+    render() {
+        const fullScreen = {
+            width: "100vw",
+            height: "100vh"
+        }
+        return (
+            <div style={fullScreen} ref={el=>this.mapContainer = el}>
+
+            </div>
+        );
+    }
+}
+
+export default MapScreen;
+
+
+
+
+
